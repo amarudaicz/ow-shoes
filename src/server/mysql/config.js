a55b902ac550bd3c0b2a00cc62b703421c2bc7e3
amaru <amarudaicz10gmail.com>
2022-11-25 21:09:56 -0300

    v3.0

--- src/server/mysql/config.js
+++ src/server/mysql/config.js
@@ -1,15 +1,14 @@
-
-const {DB_PORT, DB_HOST, DB_PASSWORD,DB_PORT,DB_USER, DB_NAME} = require('../../config')
 const mysql = require('mysql');
 
 const pool = mysql.createPool({
 
   connectionLimit:10,
-  user: DB_USER,
-  password: DB_PASSWORD,
-  database: DB_NAME,
-  host:DB_HOST ,
-  port:DB_PORT
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  host:process.env.DB_HOST ,
+  port:process.env.DB_PORT
+
 });
 
 
