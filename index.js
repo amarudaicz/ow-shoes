a8ce2a92773d2f1218efb43b499de99eca518371
amaru <amarudaicz10gmail.com>
2022-11-25 21:00:24 -0300

    v3.0

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+require('dotenv').config()
+
+
+
+ 
+//INSTANCIA DE EXPRESS
+const app = express();
+
+//MIDDLEWARES
+app.use(express.static(__dirname + '/src/server/services'));
+app.use(express.static(__dirname + '/src/client/public'));
+app.use(express.static(__dirname + '/src/server/uploads'));
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+
+
+//RUTAS
+app.use('/', require('./src/server/routes'));
+
+
+//app.set?
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/src/client/public/views/vistas');
+app.set('port', process.env.PORT );
+
+// WXw*CUULd]6eWVH=
+
+
+
+
+app.listen(app.get('port'), ( ) => {
+
+  console.log(`el server is on in ${app.get('port')}`);
+
+});
+
+
