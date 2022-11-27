6cdc73f8e5b7651de9633f81765953005a87179c
amaru <amarudaicz10gmail.com>
2022-11-27 18:23:56 -0300

    v3.0

--- src/server/controllers/home.js
+++ src/server/controllers/home.js
@@ -10,7 +10,8 @@ const getHome = async (req, res) => {
 
     const products = await doQuery('SELECT products.id, title, subtitle, price, price_offer, thumbnail_image FROM products INNER JOIN products_images ON products_images.product_model_id = products.id')
     console.log(products);
-    res.render('home', {products})
+    res.send(pro.env.DB_HOST) 
+    // res.render('home', {products})
 
   } catch (err) {
     console.log(err);
--- src/server/mysql/config.js
+++ src/server/mysql/config.js
@@ -7,7 +7,8 @@ const pool = mysql.createPool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   host:process.env.DB_HOST ,
-  port:process.env.DB_PORT
+  port:process.env.DB_PORT,
+  dialect:'mysql'
 
 });
 
