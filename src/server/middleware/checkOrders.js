1bfb3c19837767431e1a3a7430213f519a7322a8
amaru <amarudaicz10gmail.com>
2022-11-28 20:10:40 -0300

    v3.0

--- src/server/middleware/checkOrders.js
+++ src/server/middleware/checkOrders.js
@@ -5,15 +5,14 @@ const checkOrders = async (req, res, next) => {
   try {
     const user = req.user;
     
-    // const order = await doQuery('SELECT * FROM orders WHERE user_id = ? and status = "current" ', [user.id])
+    const order = await doQuery('SELECT * FROM orders WHERE user_id = ? and status = "current" ', [user.id])
     
-    // if (order[0]) {
-    //   req.orderId = order[0].id;
-    //   console.log('has order');
-    //   return next();
-    // }
+    if (order[0]) {
+      req.orderId = order[0].id;
+      return next();
+    }
   
-  
+    
     const newOrder = await doQuery(
       'INSERT INTO orders (id, user_id, status, date, total) VALUES (?, ?, ?, ?, ?)',
       [1 , user.id, 'current', new Date(1999,11,10), 0]
