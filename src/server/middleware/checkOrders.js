40b96097762dc31eba6684268c12612ff0970fd7
amaru <amarudaicz10gmail.com>
2022-11-28 12:28:29 -0300

    v3.0

--- src/server/middleware/checkOrders.js
+++ src/server/middleware/checkOrders.js
@@ -2,26 +2,30 @@ const { doQuery } = require('../services/mysqlS/operationsMysql');
 
 
 const checkOrders = async (req, res, next) => {
+  try {
+    const user = req.user;
+    
+    // const order = await doQuery('SELECT * FROM orders WHERE user_id = ? and status = "current" ', [user.id])
+    
+    // if (order[0]) {
+    //   req.orderId = order[0].id;
+    //   console.log('has order');
+    //   return next();
+    // }
   
-  const user = req.user;
   
-  // const order = await doQuery('SELECT * FROM orders WHERE user_id = ? and status = "current" ', [user.id])
+    const newOrder = await doQuery(
+      'INSERT INTO orders (user_id, status, date) VALUES (?, ?, ?)',
+      [user.id, 'current', '10-11-2000']
+    );
+    
+    req.orderId = newOrder.insertId;
   
-  // if (order[0]) {
-  //   req.orderId = order[0].id;
-  //   console.log('has order');
-  //   return next();
-  // }
-
-
-  // const newOrder = await doQuery(
-  //   'INSERT INTO orders (user_id, status, date) VALUES (?, ?, ?)',
-  //   [user.id, 'current', '10-11-2000']
-  // );
-  
-  req.orderId = user.id;
-
-  next();
+    next();
+    
+  } catch (err) {
+    res.json({error:err})
+  }
 };
 
 module.exports = { checkOrders };
